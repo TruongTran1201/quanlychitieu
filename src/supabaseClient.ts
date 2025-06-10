e370895ac0ef03d54c960bf8a7996c9946da65a9
quoctruong1201 <61037513+TruongTran1201@users.noreply.github.com>
2025-06-10 19:32:04 +0700

    change db

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Thay các giá trị bên dưới bằng thông tin dự án Supabase của bạn
-const supabaseUrl = 'https://cytspcfhwqbntgorqfbd.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5dHNwY2Zod3FibnRnb3JxZmJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1NDAzNjUsImV4cCI6MjA2NTExNjM2NX0.X0mHBnk4OLkyjnGPqosoVChMB12XMDpcGgdYZbc1Rvg'
+const supabaseUrl = 'https://jruxzwmjvtdzmdvtohpm.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpydXh6d21qdnRkem1kdnRvaHBtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1NTczMjgsImV4cCI6MjA2NTEzMzMyOH0.gRQZBw1gnO0VPdfiI-ENMjcqc3fi_k_ZTQDrGKF1MNM'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
